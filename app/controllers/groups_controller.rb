81d5c133e047498f646ee4ebff8775e3106151ae
jkxruby <jkxruby@gmail.com>
2017-08-12 22:43:24 +0800

    add permission to user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new, :edit, :destroy, :update, :show]
+  before_action :find_group_and_check_permission, only: [:update, :edit, :destroy]
 def index
   @groups = Group.all
 end
@@ -18,11 +19,10 @@ def create
 end
 
 def edit
-  @group = Group.find(params[:id])
+  @group.user = current_user
 end
 
 def update
-  @group = Group.find(params[:id])
   if @group.update(group_params)
     redirect_to groups_path, notice: "update success"
   else
@@ -35,12 +35,19 @@ def show
 end
 
 def destroy
-  @group = Group.find(params[:id])
   @group.destroy
   redirect_to groups_path, alert: "deleted"
-end 
+end
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "you have no permission"
+    end
+  end
+
   def group_params
     params.require(:group).permit(:title, :description)
   end
